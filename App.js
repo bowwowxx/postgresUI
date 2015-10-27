fa8a386bf8b3450874e88eed8a2de02cb234762b
bowwow <jummykimo@gmail.com>
2015-10-27 16:45:55 +0800

    for metadata

--- App.js
+++ App.js
@@ -41,7 +41,7 @@ app.set('port', process.env.PORT || 8080);
 
 app.use(express.static(__dirname + '/client'));
 //app.use(express.errorHandler());
-app.use(errorHandler);
+//app.use(errorHandler);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
--- dbOperations.js
+++ dbOperations.js
@@ -4,12 +4,12 @@ module.exports = {
 
         //You can run command "heroku config" to see what is Database URL from Heroku belt
 
-        var conString = process.env.DATABASE_URL || "postgres://postgre:postgre@xx.xx.xx.xx:5432/postgre";
+        var conString = process.env.DATABASE_URL || "postgres://postgres:1qaz2wsx@xx.xx.xx.xx:xx/postgres";
         var client = new pg.Client(conString);
 
         client.connect();
 
-        var query = client.query("select * from employee");
+        var query = client.query("select * from song");
 
         query.on("row", function (row, result) {
             result.addRow(row);
@@ -27,13 +27,20 @@ module.exports = {
     addRecord : function(req, res){
         var pg = require('pg');
 
-        var conString = process.env.DATABASE_URL ||  "postgres://postgre:postgre@xx.xx.xx.xx:5432/postgre";
+        var conString = process.env.DATABASE_URL ||  "postgres://postgres:1qaz2wsx@xx.xx.xx.xx:xx/postgres";
         var client = new pg.Client(conString);
 
         client.connect();
-        var query = client.query("insert into employee (firstName,lastName,email,mobile) "+
-                                "values ('"+req.query.fName+"','"+req.query.lName+"','"+
-                                    req.query.email+"','"+req.query.mbl+"')");
+
+        var query = client.query("insert into song (title,len,lang,artist,artist_keywords,gender,country,genre,subtitle,release_date,add_date,artist_pic_url,lyrics,composer,rank,propoties) "+
+                                "values ('"+req.query.title+"','"+req.query.len+"','"+
+                                    req.query.lang+"','"+req.query.artist+"','"+
+                                    req.query.artist_keywords+"','"+req.query.gender+"','"+
+                                    req.query.country+"','"+req.query.genre+"','"+
+                                    req.query.subtitle+"','"+req.query.release_date+"','"+
+                                    req.query.add_date+"','"+req.query.artist_pic_url+"','"+
+                                    req.query.lyrics+"','"+req.query.composer+"','"+
+                                    req.query.rank+"','"+req.query.propoties+"')");
 
         query.on("end", function (result) {
             client.end();
@@ -46,12 +53,12 @@ module.exports = {
      delRecord : function(req, res){
         var pg = require('pg');
 
-        var conString = process.env.DATABASE_URL ||  "postgres://postgre:postgre@xx.xx.xx.xx:5432/postgre";
+        var conString = process.env.DATABASE_URL ||  "postgres://postgres:1qaz2wsx@xx.xx.xx.xx:xx/postgres";
         var client = new pg.Client(conString);
 
         client.connect();
 
-        var query = client.query( "Delete from employee Where id ="+req.query.id);
+        var query = client.query( "Delete from song Where id ="+req.query.id);
 
         query.on("end", function (result) {
             client.end();
@@ -64,18 +71,30 @@ module.exports = {
     createTable : function(req, res){
         var pg = require('pg');
 
-        var conString = process.env.DATABASE_URL ||  "postgres://postgre:postgre@xx.xx.xx.xx:5432/postgre";
+        var conString = process.env.DATABASE_URL ||  "postgres://postgres:1qaz2wsx@xx.xx.xx.xx:xx/postgres";
         var client = new pg.Client(conString);
 
         client.connect();
 
-        var query = client.query( "CREATE TABLE employee"+
+        var query = client.query( "CREATE TABLE song"+
                                     "("+
-                                      "firstname character varying(50),"+
-                                      "lastname character varying(20),"+
-                                      "email character varying(30),"+
-                                      "mobile character varying(12),"+
-                                      "id serial NOT NULL"+
+                                    	"id serial NOT NULL,"+
+                                    	"title varchar(128),"+
+                                    	"len int2,"+
+                                    	"lang varchar(32),"+
+                                    	"artist varchar(256),"+
+                                    	"artist_keywords varchar(256),"+
+                                    	"gender varchar(32),"+
+                                    	"country varchar(32),"+
+                                    	"genre varchar(32),"+
+                                    	"subtitle varchar(256),"+
+                                    	"release_date date,"+
+                                    	"add_date date,"+
+                                    	"artist_pic_url varchar(1024),"+
+                                    	"lyrics varchar(256),"+
+                                    	"composer varchar(256),"+
+                                    	"rank int2,"+
+                                    	"propoties int2"+
                                     ")");
 
         query.on("end", function (result) {
@@ -89,12 +108,12 @@ module.exports = {
     dropTable : function(req, res){
         var pg = require('pg');
 
-        var conString = process.env.DATABASE_URL || "postgres://postgre:postgre@xx.xx.xx.xx:5432/postgre";
+        var conString = process.env.DATABASE_URL || "postgres://postgres:1qaz2wsx@xx.xx.xx.xx:xx/postgres";
         var client = new pg.Client(conString);
 
         client.connect();
 
-        var query = client.query( "Drop TABLE employee");
+        var query = client.query( "Drop TABLE song");
 
         query.on("end", function (result) {
             client.end();
